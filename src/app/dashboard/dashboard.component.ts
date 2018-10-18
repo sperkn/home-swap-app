bb4021c35b6d28cb9f4c73f132e2185cc0ac3a11
Nicholas Kreps <nicholas.kreps@gmail.com>
2018-10-18 01:51:30 -0400

    dashboard updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,7 +15,6 @@ export class DashboardComponent implements OnInit {
   travels: any = [];
   connections: any = [];
   userId: any;
-  // noData: number=
 
   constructor(
     private travel: TravelRoutesService,
